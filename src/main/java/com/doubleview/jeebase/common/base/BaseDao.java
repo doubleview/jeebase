1f77b3f52da3c17b9839d79bfec452a8a3ee37ec
kaen <1078828966@qq.com>
2016-08-27 13:11:57 +0800

    add some comments

--- src/main/java/com/doubleview/jeebase/common/base/BaseDao.java
+++ src/main/java/com/doubleview/jeebase/common/base/BaseDao.java
@@ -9,29 +9,29 @@ public interface BaseDao<T> {
 
     /**
      * 获取单条数据
-     * @param id
-     * @return
+     * @param id 实体类主键
+     * @return 实体对象
      */
      T get(String id);
 
     /**
      * 获取单条数据
-     * @param entity
-     * @return
+     * @param entity 实体对象
+     * @return 实体对象
      */
      T get(T entity);
 
     /**
      * 查询数据列表，如果需要分页，请设置分页对象，如：entity.setPage(new Page<T>());
-     * @param entity
-     * @return
+     * @param entity 实体对象
+     * @return 实体对象列表
      */
      List<T> findList(T entity);
 
 
     /**
      * 插入数据
-     * @param entity
+     * @param entity 实体对象
      * @return
      */
      int insert(T entity);
